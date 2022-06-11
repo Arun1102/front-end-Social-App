c782ab01de129ad1c4238f09a66c96f7d0c5a134
Arun1102 <arunattappan@gmail.com>
2022-06-11 15:46:48 +0800

    second setup domainmodel

---
+++ source/repos/ProjectPhase1/ProjectPhase1/Models/Dashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectPhase1.Models
+{
+    public class Dashboard
+    {
+        public int dashboardID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string anchortag { get; set; }
+        public string imgtag { get; set; }
+    }
+}
